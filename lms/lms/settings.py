96fe3fa572975b54e7f264387a65020a756b4e09
Joel Masiaga <masiagajoel001@gmail.com>
2026-04-16 00:22:51 +0300

    feat: initialize Django project settings with environment-based configuration, Cloudinary storage, and third-party integrations

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -153,12 +153,12 @@ SITE_ID = 1
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 #Database connection
 POSTGRES_LOCALLY = True
